5d5a467700736818a0d4cc9f42bc3df88942c294
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-07 09:43:04 +0530

    Netflix - Created the User Model

---
+++ src/main/java/com/netflix/model/User.java
@@ -0,0 +1,20 @@
+package com.netflix.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Profile> profiles;
+}
