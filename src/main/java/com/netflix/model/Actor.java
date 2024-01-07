e31489627739b93abc7a9a20ee4053911ba3feb7
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-07 09:47:21 +0530

    Netflix - Created the Actor Model

---
+++ src/main/java/com/netflix/model/Actor.java
@@ -0,0 +1,17 @@
+package com.netflix.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long actorId;
+
+    private String name;
+
+    @ManyToMany
+    private List<Video> videos; // Assuming videoId is a Long
+}
